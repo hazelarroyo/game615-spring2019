bfe487c41944312b6a11631bf1b0ba7d7e7c808f
Hazel Arroyo <hazel.arroyo94@gmail.com>
2019-02-06 01:54:16 -0500

    Exercise 03 Push

---
+++ exercises/exercise03/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+
+{
+
+    public Text countText;
+    public Text winText;
+
+    public Rigidbody rb;
+    public float speed = 250f;
+
+    private int count;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+    }
+
+    //Adds movement with default key inputs and adds force to movement
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed * Time.deltaTime);
+    }
+
+    //Trigger collider to pick up collectable items in scene - doesn't destroy 
+    //objects; deactivates the objects
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count:" + count.ToString();
+        if (count >= 33) 
+        {
+            winText.text = "You Win";
+        }
+
+    }
+}
