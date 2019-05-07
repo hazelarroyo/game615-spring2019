0945ed86acdba750a497e7816ff8003becb668d6
Hazel Arroyo <hazel.arroyo94@gmail.com>
2019-05-07 07:48:58 -0400

    Added Health

---
+++ exercises/final/Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Image bar;
+    public float fill;
+    private static float healthPoints;
+
+    void Start()
+    {
+        fill = 1f; 
+    }
+
+    void Update()
+    {
+        healthPoints = PlayerController.healthPoints;
+        bar.fillAmount = healthPoints * .01f;
+    }
+}
