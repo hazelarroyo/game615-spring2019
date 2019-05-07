5989bd6419490c86a9372921f63a740569985d2b
Hazel Arroyo <hazel.arroyo94@gmail.com>
2019-05-07 13:07:12 -0400

    Animations Added - Final Edits Done

---
+++ exercises/final/Assets/Scripts/PlatformAttach.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformAttach : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Enemy;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = transform;
+        }
+
+        if (other.gameObject == Enemy)
+        {
+            Enemy.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+        }
+    }
+}
