8687cf5f0f7640dfe3f2ad5957b5f0c8dd89f6bf
Hazel Arroyo <hazel.arroyo94@gmail.com>
2019-05-07 09:23:11 -0400

    Successful Debug of EndTriggers

---
+++ exercises/final/Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndTrigger : MonoBehaviour
+{
+
+    void Update()
+    {
+        if(PlayerController.healthPoints < 0f)
+        {
+            SceneManager.LoadScene("endScreen");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player has entered trigger.");
+            UpdateEnd();
+        }
+    }
+
+    void UpdateEnd()
+    {
+        SceneManager.LoadScene("winScreen");
+    }
+}
